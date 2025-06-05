2b9786039c8cd9df831ea00b898bfbc25e3d5d8e
5jun99 <oj041088@gmail.com>
2025-06-05 13:25:57 +0900

    fix: 경로 수정

--- service/backend/service/fastapi-server/src/llm/init_store.py
+++ service/backend/service/fastapi-server/src/llm/init_store.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def load_vectorstore(path: str = "fashion_knowledge_db"):
+def load_vectorstore(path: str = "src/llm/fashion_knowledge_db"):
     embedding = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
     return FAISS.load_local(path, embedding, allow_dangerous_deserialization=True)
